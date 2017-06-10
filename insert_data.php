7383397c4fca1287b61f449b479b361ac476ff6a
carikka <carissadean@hotmail.com>
2017-06-10 07:31:11 +0100

    Forgot to add connect #Covfefe

--- insert_data.php
+++ insert_data.php
@@ -1,5 +1,7 @@
 <?php
 
+include('config.php')
+
 $sqlinsert = "INSERT INTO Volunteers (first_name, last_name, age, gender, email, phone, location, preference, nok_first_name, nok_last_name, nok_phone, nok_relationship) 
 VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[age]', '$_POST[gender]', '$_POST[emailaddress]', '$_POST[phonenumber]', '$_POST[address]', '$_POST[preference]', '$_POST[nextofkinfirstname]', '$_POST[nextofkinlastname]', '$_POST[nextofkinphonenumber]', '$_POST[nextofkinrelationship]')"
 	
