aa4e155c2b83966bbb3026f2c62350fc2eab5166
carikka <carissadean@hotmail.com>
2017-06-10 09:45:41 +0100

    Updated signup and insert_data

--- insert_data.php
+++ insert_data.php
@@ -1,15 +1,33 @@
 <?php
 
-include('config.php')
+	include('config.php'); 
 
-$sqlinsert = "INSERT INTO Volunteers (first_name, last_name, age, gender, email, phone, location, preference, nok_first_name, nok_last_name, nok_phone, nok_relationship) 
-VALUES ('$_POST[firstname]', '$_POST[lastname]', '$_POST[age]', '$_POST[gender]', '$_POST[emailaddress]', '$_POST[phonenumber]', '$_POST[address]', '$_POST[preference]', '$_POST[nextofkinfirstname]', '$_POST[nextofkinlastname]', '$_POST[nextofkinphonenumber]', '$_POST[nextofkinrelationship]')"
+
+	if(isset($_POST['submit'])) {
+				
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$age = $_POST['age'];
+		$gender = $_POST['gender'];
+		$email = $_POST['emailaddress'];
+		$phone = $_POST['phonenumber'];
+		$location = $_POST['address'];
+		$preference = $_POST['preference'];
+		$nok_first_name = $_POST['nextofkinfirstname'];
+		$nok_last_name = $_POST['nextofkinlastname'];
+		$nok_phone = $_POST['nextofkinphonenumber'];
+		$nok_relationship = $_POST['nextofkinrelationship'];
+
+
+		$sqlinsert = "INSERT INTO `Volunteers` (first_name, last_name, age, gender, email, phone, location, preference, nok_first_name, nok_last_name, nok_phone, nok_relationship) 
+		VALUES ('$firstname', '$lastname', '$age', '$gender', '$email', '$phone', '$location', '$preference', '$nok_first_name', '$nok_last_name', '$nok_phone', '$nok_relationship')";
 	
-	if (!mysqli_query ($connect, $sqlinsert)) {
-		die("Error - not submitted");
 		
-	}
-	
-	echo "Thank you for signing up!";
+		if (mysqli_query($db, $sqlinsert)) {
+			echo "Thank you for signing up";
+		} else {
+			echo "Error - could not sign up";
+		}
 
+	} 
 ?>
